68703b90f1d6d297c3d4799c51efcbeb2c45c07c
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-16 12:32:28 +0300

    Add OrderServiceTests class and update test project config
    - Introduced `OrderServiceTests` class as a placeholder for future unit tests.
    - Updated `Talabat.Test.csproj` to target .NET 8.0 with implicit usings and nullable reference types.
    - Added necessary package references for testing, including `Moq`, `xunit`, and `coverlet.collector`.
    - Included project references to `Talabat.Core`, `Talabat.Service`, and `Talabat`.

---
+++ Talabat.Test/Application/Services/OrderServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using Stripe.Climate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Service;
+
+namespace Talabat.Test.Application.Services
+{
+    public class OrderServiceTests
+    {
+       
+    }
+}
